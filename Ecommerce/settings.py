b89b98424ab362df92cdcb4bb76a5c1e4f9a8d72
aliqnbri <aliqnbri1998@gmail.com>
2024-05-05 19:16:23 +0330

    Build - account app created.

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -22,6 +22,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Internal apps
+    'account.apps.AccountConfig',
 ]
 
 MIDDLEWARE = [
