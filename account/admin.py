982777b5f6a6f66ed271a26a7496d66fc484a787
aliqnbri <aliqnbri1998@gmail.com>
2024-07-11 20:28:47 +0330

    Develep: admin.py for Regestring users in panel

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from core.managers import export_to_csv
+from typing import List, Tuple ,Type ,Callable
+from account.models import CustomerProfile
+
+User = get_user_model()
+
+
+class CustomUserAdmin(UserAdmin):
+    """
+    The forms to add and change user instances
+    """
+    form: Type[UserAdminChangeForm] = UserAdminChangeForm
+    add_form: Type[UserAdminCreationForm] = UserAdminCreationForm
+
+    list_display: List[str] = ['username', 'email', 'role']
+    list_filter: List[str] = ['role',]
+    fieldsets: Tuple[Tuple[str, dict], Tuple[str, dict], Tuple[str, dict]] = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ()}),
+        ('Permissions', {'fields': ('is_active', 'role')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password', 'password_2')}
+         ),
+    )
+    search_fields: List[str] = ['username', 'email']
+    ordering: str = '-created'
+    list_per_page = 10
+    filter_horizontal: Tuple = ()
+    actions = [export_to_csv]
+
+
+admin.site.register(User, CustomUserAdmin)
+
--- account/models.py
+++ account/models.py
@@ -1,17 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils.translation import gettext_lazy as _
 from core.models import BaseModel
 
 # Create your models here.
 
 class CustomUser(AbstractBaseUser, PermissionsMixin, BaseModel):
     class Role(models.TextChoices):
-        ADMIN = 'admin', 'Admin'
-        OPERATOR = 'operator', 'Operator'
-        CUSTOMER = 'customer', 'Customer'
-        GUEST = 'guest', 'Guest'
+        ADMIN = 'admin', _('Admin')
+        OPERATOR = 'operator', _('Operator')
+        CUSTOMER = 'customer', _('Customer')
+        GUEST = 'guest', _('Guest')
 
     email = models.EmailField(max_length=255, unique=True)
+    phone_number = models.CharField(null=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     role = models.CharField(max_length=8, choices=Role, default=Role.CUSTOMER)
