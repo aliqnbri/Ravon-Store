a0cd3f9171d83bd1b648e3fda699c35dc4c13a05
aliqnbri <aliqnbri1998@gmail.com>
2024-07-15 21:31:54 +0330

    Dev: product Mixin Veiw

--- product/views.py
+++ product/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from rest_framework import generics , mixins
+from product.models import Product , Category ,Review
+from product.serializers import ProductSerializer , CategorySerializer
 
 # Create your views here.
+
+class ProductMixinView(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    generics.GenericAPIView
+):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'slug'
+
+    def get_queryset(self):
+        return super().get_queryset()
+    
+
+    def get(self,request, *args,**kwargs):
+        slug = self.kwargs.get('slug')
+        if slug is not None:
+            return self.retrieve(request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+    
+    def perform_create(self, serializer):
+        return super().perform_create(serializer)
