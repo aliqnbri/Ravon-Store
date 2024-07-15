5552ac64a7f932b1b596bf1b7cfc1c111c897a78
aliqnbri <aliqnbri1998@gmail.com>
2024-07-15 21:11:10 +0330

    Dev: Product Model

--- product/models.py
+++ product/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from core.models import BaseModel
 from django.utils.text import slugify
+from account.models import CustomerProfile
 
 # Create your models here.
 
@@ -35,6 +36,13 @@ class Category(BaseModel):
         super().save(**kwargs)
 
 
+class Brand(BaseModel):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
 class Review(BaseModel):
     """
     A Django model representing a review for a specific book.
@@ -46,4 +54,39 @@ class Review(BaseModel):
 
     def __str__(self):
         return f"{self.CustomerProfile.username} commets:{self.comment}"
+    
+
+class Product(BaseModel):
+
+    """
+    A Django model representing a specific product in an online bookstore.
+    """
+
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    brand = models.ForeignKey(Brand,on_delete=models.CASCADE )
+    image = models.ImageField(upload_to='media/products/', blank=True, null=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    review = models.ForeignKey(Review, null=True,blank=True, on_delete=models.CASCADE, related_name='reviews')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')    
+    
+    class Meta:
+        ordering = ('name',)
+
+
+    def save(self, **kwargs):
+        """
+        Overridden save method to create a unique slug based on the name.
+        """
+        self.slug = slugify(self.name)
+        super().save(**kwargs)
+
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/{self.category.slug}/{self.slug}/'    
 
