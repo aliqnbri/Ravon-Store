f6be6f485cb46733b1d916f4c4f6fdc976ad4214
aliqnbri <aliqnbri1998@gmail.com>
2024-07-16 14:37:55 +0330

    Register Order Model

--- order/admin.py
+++ order/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+from core.managers import export_to_csv
+from .models import Order, OrderItem
+
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ['product']
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['customer',
+                    'created_at', 'updated_at']
+    list_filter = ['created_at', 'updated_at',]
+    inlines = [OrderItemInline]
+    actions = [export_to_csv]
