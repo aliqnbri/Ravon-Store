cb78b4ced6a3a35aeae0d590985f8e1bfbf1c079
aliqnbri <aliqnbri1998@gmail.com>
2024-07-18 10:17:14 +0330

    style: admin register

--- account/admin.py
+++ account/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from account.models import CustomUser , CustomerProfile , Address
-from account.forms import RegisterUserForm, EditUserForm
+from account.models import CustomUser , Address
+from account.forms import CustomUserCreationForm , CustomUserChangeForm
 from django.contrib.auth.admin import UserAdmin
 from core.managers import export_to_csv
-from typing import List, Tuple, Type
+from django.utils.translation import gettext_lazy as _
+
 
 admin.site.site_header = 'Ali Qanbari Admin'
 admin.site.site_title = 'Ali Qanbari'
@@ -12,9 +13,39 @@ admin.site.index_title = 'Ali Qanbari administration'
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
+    form = CustomUserChangeForm
+    add_form= CustomUserCreationForm
+    model = CustomUser
+    list_display = ('full_name','email', 'role', 'is_active',)
+    list_filter = ('is_staff', 'is_active','role' ,'staff_role')
+    fieldsets = (
+        (_('Account Info'), {'fields': ('id', 'username', 'email', 'phone_number')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (_('Privileges'), {
+            'fields': ('role', 'staff_role', 'is_active', 'is_verified'),
+        }),
+        (_('Permissions'), {
+            'fields': ('groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'role', 'staff_role', 'is_active',)}
+         ),
+    )
+    readonly_fields = ['is_verified', 'id']
+    search_fields = ('email', 'username', 'phone_number')
+    ordering = ('email',)
+    filter_horizontal = ()
 
-    form: Type[EditUserForm] = EditUserForm
-    add_form: Type[RegisterUserForm] = RegisterUserForm
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(CustomUserAdmin, self).get_inline_instances(request, obj)
+
 
 
 
