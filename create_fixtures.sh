d2c1092bb0a33b2a1357be8617e7436c04c3851d
aliqnbri <aliqnbri1998@gmail.com>
2024-08-07 21:42:15 +0330

    Fix: bash scripts

--- create_fixtures.sh
+++ create_fixtures.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+
 # Set the project directory
-PROJECT_DIR=$(dirname $(dirname $(readlink -f $0)))
+PROJECT_DIR=$(dirname $(dirname $(dirname $0 | tr -d '\n')))echo "Project directory: $PROJECT_DIR"
 
 # Change to the project directory
 cd "$PROJECT_DIR"
 
+
 # Get a list of all apps in the project
-APPS=$(python manage.py showmigrations | awk '{print $1}' | grep -v '\<')
+APPS="account product core customer payment order"
 
 # Create fixtures directory for each app and dump data to fixtures files
 for APP in $APPS; do
   FIXTURES_DIR=$APP/fixtures
-  mkdir -p $FIXTURES_DIR
-  echo "Dumping data for $APP to $FIXTURES_DIR..."
-  python manage.py dumpdata $APP --indent=2 --output=$FIXTURES_DIR/$APP.json
+  mkdir -p "$FIXTURES_DIR"
+  python manage.py dumpdata $APP --indent=2 --output="$FIXTURES_DIR/$APP.json"
+  echo "Dumped data for $APP"
 done
--- django_migration_script.sh
+++ django_migration_script.sh
@@ -6,10 +6,6 @@ python manage.py makemigrations account
 python manage.py makemigrations customer
 python manage.py makemigrations product
 python manage.py makemigrations order
-
 python manage.py makemigrations payment
 python manage.py migrate
 
-# Run loaddata commands
-python manage.py loaddata account/fixtures/account.json
-python manage.py loaddata product/fixtures/product.json
