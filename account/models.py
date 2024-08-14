93a14e1e59f1865eb11e27304efedde26bea86af
aliqnbri <aliqnbri1998@gmail.com>
2024-08-14 20:24:43 +0330

    rebuild cusotmer_profile

--- account/models.py
+++ account/models.py
@@ -61,6 +61,11 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     def is_customer(self) -> bool:
         """Check if the user is a customer."""
         return self.role == self.Role.CUSTOMER
+    
+    @property
+    def full_name(self) -> str:
+        """Returns the user's full name"""
+        return f"{self.first_name} {self.last_name}"
 
     def save(self, *args, **kwargs) -> None:
         """Override the save method to add custom logic."""
--- customer/models.py
+++ customer/models.py
@@ -11,10 +11,12 @@ from django.contrib.contenttypes.models import ContentType
 # Create your models here.
 
 
+
 class Address(BaseModel):
     """
     Address Model for user's address 
     """
+    customer_profile = models.ForeignKey('CustomerProfile', on_delete=models.CASCADE)
     country = models.CharField(max_length=200, null=True, blank=True)
     state = models.CharField(max_length=200, null=True, blank=True)
     city = models.CharField(max_length=50)
@@ -25,15 +27,23 @@ class Address(BaseModel):
     longitude = models.DecimalField(
         max_digits=9, decimal_places=2, null=True, blank=True)
     secondary_address = models.TextField(null=True, blank=True)
+    is_default = models.BooleanField(default=False)
 
-    def get_address(self):
-        return f"Address : {self.city} ,{self.street}, {self.postal_code}"
 
-    def get_full_address(self):
-        return f"{self.country},{self.city}, {self.state},{self.street}, {self.postal_code}"
+
+
+    def get_full_address(self) -> str:
+        """Returns the full address in a formatted string"""
+        return f"Address: {self.country},{self.city}, {self.state},{self.street}, {self.postal_code}"
+
+    def __str__(self) -> str:
+        return self.get_full_address()
 
 
 class CustomerProfile(models.Model):
+    """
+    Customer Profile Model
+    """
     class Gender(models.TextChoices):
         MALE = 'male', _('Male')
         FEMALE = 'female', _('Female')
@@ -46,22 +56,27 @@ class CustomerProfile(models.Model):
         max_length=6, choices=Gender.choices, null=True, blank=True)
     avatar = models.ImageField(
         upload_to='media/avatars/', null=True, blank=True)
-    address = models.ForeignKey(
-        Address, null=True, blank=True, on_delete=models.CASCADE)
 
     @admin.display
     def full_name(self):
-        return self.first_name + " " + self.last_name
+        return self.customer.first_name + " " + self.customer.last_name
+    
 
-    @property
-    def get_username(self):
-        return f'{self.username}'
+    
+    def get_default_address(self) -> Address:
+        """Returns the customer's default address"""
+        if (default_address := self.address_set.filter(is_default=True).first()):
+            return default_address.get_full_address()
+        return None
     
     def get_default_address(self):
         return self.address_set.filter(is_default=True).first()
     
-    def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+    def __str__(self) -> str:
+        return f"{self.customer.first_name} {self.customer.last_name}"
+
+
+
 
 
 class Comment(BaseModel):
